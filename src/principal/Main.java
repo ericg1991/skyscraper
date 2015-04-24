5f30de92599322e4c3fd8b81f7585dd776d40555
ericg1991 <ericg@live.it>
2015-04-24 13:25:27 +0200

    First version of the scraper. I give the URL to it, for now it scrapes just the first page

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,68 +1,193 @@
 package principal;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.commons.logging.LogFactory;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
+import com.gargoylesoftware.htmlunit.ThreadedRefreshHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptJobManager;
 
 class Main {
-		
+
+	static {
+		// disable HtmlUnit logging
+		LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log",
+				"org.apache.commons.logging.impl.NoOpLog");
+	}
+
 	public static void main(String args[]) throws IOException {
+
+		String URL = "http://www.skyscanner.it/trasporti/voli/bgy/mad/150522/150529/tariffe-aeree-da-milano-bergamo-orio-al-serio-per-madrid-a-maggio-2015.html?adults=1&children=0&infants=0&cabinclass=economy&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&rtn=1";
+		WebClient webClient = new WebClient(BrowserVersion.FIREFOX_31);
+		webClient.getOptions().setJavaScriptEnabled(true);
+		webClient.getOptions().setRedirectEnabled(true);
+		webClient.getOptions().setCssEnabled(true);
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		webClient.getOptions().setThrowExceptionOnScriptError(true);
+		webClient.getOptions().setUseInsecureSSL(true);
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+		webClient.waitForBackgroundJavaScriptStartingBefore(10000);
+		webClient.setJavaScriptTimeout(2147483647);
+		webClient.setCssErrorHandler(new SilentCssErrorHandler());
+		webClient.waitForBackgroundJavaScript(5000);
+		webClient.setRefreshHandler(new ThreadedRefreshHandler());
+		webClient.getCookieManager().setCookiesEnabled(true);
 		
-		String URL = "http://www.skyscanner.it/trasporti/voli/bgy/mad/150610/150615/tariffe-aeree-da-milano-bergamo-orio-al-serio-per-madrid-a-giugno-2015.html?adults=1&children=0&infants=0&cabinclass=economy&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&rtn=1&PageSpeed=noscript";
+		Elements otheragencies = new Elements();
+		Elements bestagenciesprices = new Elements();
+		Elements bestagencies = new Elements();
+		Elements flights = new Elements();
+		Elements departuretime = new Elements();
+		Elements arrivaltime = new Elements();
+		Date date = new Date();
 		
-		
-		//A list of Elements, with methods that act on every element in the list.
-		//An HTML element consists of a tag name, attributes, and child nodes (including text nodes and other elements).
-		//From an Element, you can extract data, traverse the node graph, and manipulate the HTML.
-		Elements agencies = new Elements();
-		
-		//A HTML Document.
-		Document doc = new Document("");
-		
-		int i = 1;
-		//String URL = "http://www.skyscanner.it/trasporti/voli/bgy/mad/150610/150615/tariffe-aeree-da-milano-bergamo-orio-al-serio-per-madrid-a-giugno-2015.html?adults=1&children=0&infants=0&cabinclass=economy&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&rtn=1";
-		System.out.println(i + "; " + URL);
-		
-		
-		
-		try{
-			PrintWriter writer = new PrintWriter("agencies.txt");
-			// Carica la pagina web
-			try{
-				//userAgent --> Request configuration can be made using the shortcut methods in Connection
-				//Timeout -->  Set the request timeouts (connect and read).
-				doc = Jsoup.connect(URL).userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").timeout(20000).get();
-				}
-			catch (IOException e) {
-				System.err.println("Errore di lettura della pagina da internet");
+		HtmlPage page = null;
+		try {
+			page = webClient.getPage(URL);
+		} catch (Exception e) {
+			System.out.println("Get page error");
+		}
+		JavaScriptJobManager manager = page.getEnclosingWindow()
+				.getJobManager();
+		int timeToWait = 20;
+		while (manager.getJobCount() > 0) {
+			timeToWait--;
+			System.out.println(timeToWait + " seconds left... ("
+					+ manager.getJobCount() + " jobs left)\n");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			//stampa ciò che è contenuto in doc ovvero la pagina html scaricata (questo è per provare e vedere se prende la pagina giusta, non andrà nel programma finale)
-			writer.println(doc.toString());
-			
-			
-			//Public Elements select(String cssQuery)
-			//Find elements that match the Selector CSS query,
-			//with this element as the starting context. Matched elements may include this element, or any of its children
-			agencies = doc.select("a.ticketing-agent.mainquote-agent");
-			
-			
-			
-			//.text() -> Prende l'elemento di testo presente tra i tag
-			for (Element el : agencies) {
-				writer.println(el.text());
+			if (timeToWait <= 0)
+				break;
+		}
+		
+		// save all contents
+		PrintWriter wr = new PrintWriter("PageAsXml.html");
+		wr.println(page.asXml());
+		wr.close();
+		
+		PrintWriter wr1 = new PrintWriter("PageAsText.txt");
+		wr1.println(page.asText());
+		wr1.close();
+		
+		File input = new File("/Users/Eric/Documents/workspace/ProvaSkyScrape/PageAsXml.html");
+		Document doc = Jsoup.parse(input, "UTF-8", "http://skyscanner.it/");		
+		
+		PrintWriter writer = new PrintWriter("agencies.txt");
+		
+		//salvo gli elementi di cui abbiamo bisogno da cui estrarre le informazioni
+		flights = doc.getElementsByClass("airline");
+		departuretime = doc.select("div.depart");
+		arrivaltime = doc.select("div.arrive");
+		bestagencies = doc.select("div.mainquote-wrapper.clearfix").select("a.ticketing-agent.mainquote-agent");
+		bestagenciesprices = doc.select("div.mainquote-wrapper.clearfix").select("a.mainquote-price.big");
+		otheragencies = doc.select("div.details-group-altquotes-wrapper").select("ul");
+		
+		//dichiaro le liste che servono per salvarvi all'interno le informazioni necessarie poi alla stampa a schermo
+		ArrayList<String> timelistgo = new ArrayList<String>();
+		ArrayList<String> timelistback = new ArrayList<String>();
+		ArrayList<String> bestagencieslist = new ArrayList<String>();
+		ArrayList<String> bestagenciespriceslist = new ArrayList<String>();
+		ArrayList<String> flightslist = new ArrayList<String>();
+		
+		int b=0;
+		int i;
+		int j;
+		int t;
+		
+		//creo una lista di elementi che sono i voli (non serve posso stampare direttamente)
+//		for (Element el : flights) {
+//			flightslist.add(el.text());
+//		}
+		
+		//creo una lista con tutte le partenze di tutti i voli
+		for (Element el : departuretime) {
+				timelistgo.add(el.text());
+				if (timelistgo.get(b).contains("Andata")){
+					timelistgo.remove(b);
+					}
+				else{
+				b++;
+				}
+			}
+		
+		//creo una lista con tutti gli arrivi di tutti i voli
+		for (Element el : arrivaltime) {
+			timelistback.add(el.text());
+			}
+		
+		//creo una lista delle agenzie con i migliori prezzi per ciascuno volo (solo nome agenzia)
+		for (Element el : bestagencies) {
+			bestagencieslist.add(el.text());
 			}
 			
-			
-			// Chiudi il file di output prima di uscire 
-			writer.close();
+		//creo una lista delle agenzie con i migliori prezzi per ciascuno volo (solo prezzo agenzia)
+		for (Element el : bestagenciesprices) {
+			bestagenciespriceslist.add(el.text());
+			}
 
-			
-			}catch(Exception FileNotFoundException){}
+		//estraggo le informazioni dagli elementi(o dalle liste che ho creato prima) e le stampo a schermo
+		
+		//scrive data e ora della query al sito
+		writer.println("Research time: " + date.toString());
+		writer.println();
+		//scrive i voli disponibili per la tratta
+		writer.println("Flights with their information:");
+		writer.println();
+		for(i=0; i<flights.size(); i++){
+			writer.println(i+1 + ". " + flights.get(i).text());
+			//scrive orari partenza di andata e ritorno allineati (con lista)
+			for (t=0; t<2; t++) {
+				writer.println(timelistgo.get(t).toString() + " -> " + timelistback.get(t));
+			}
+			writer.println();
+			// scrive di ogni possibile volo l'agenzia che offre il prezzo migliore
+			writer.println("Agencies selling this flight are:");
+			writer.println("> " + bestagencieslist.get(i) + " " + bestagenciespriceslist.get(i));
+			//di ogni volo scrive le altre agenzie con prezzi peggiori rispetto alla migliore			
+			for (j = 0; j < otheragencies.get(i).select("a").size(); j++) {
+//				String[] words = otheragencies.get(i).select("a").get(j).text().split(" ");
+//				writer.println("> " + words[0] + " " + words[1] + " " + words[2]);
+				writer.println("> " + otheragencies.get(i).select("a").get(j).text());
+			}
+			writer.println();
+		}
+	
+	
+		writer.close();
+		
+		// save just the div "content-main"
+		HtmlDivision div = page.getHtmlElementById("content-main");
+		div.asText();
+
+		wr = new PrintWriter("DivAsXml.txt");
+		wr.println(div.asXml());
+		wr.close();
+
+		wr = new PrintWriter("DivAsText.txt");
+		wr.println(div.asText());
+		wr.close();
+
+		webClient.close();
+
+		System.out.println("Done.");
+
 	}
 }
